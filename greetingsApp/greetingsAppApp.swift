73ea2a66aced770fdee841c4b5c2470703dd070b
Kamila <ksalimzh@gmail.com>
2025-03-02 15:07:59 +0500

    Initial Commit

---
+++ greetingsApp/greetingsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  greetingsAppApp.swift
+//  greetingsApp
+//
+//  Created by kamila on 02.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct greetingsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
