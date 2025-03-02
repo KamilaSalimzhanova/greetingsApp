2c44adafdb5caf71401474d5deb2e0efb3a6c0d5
Kamila <ksalimzh@gmail.com>
2025-03-02 15:45:08 +0500

    vstack and zstack for background

--- greetingsApp/ContentView.swift
+++ greetingsApp/ContentView.swift
@@ -9,13 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(
+                colors: [
+                    Color(red: 230 / 255.0, green: 240 / 255.0, blue: 255 / 255.0),
+                    Color(red: 200 / 255.0, green: 230 / 255.0, blue: 255 / 255.0),
+                    Color(red: 210 / 255.0, green: 250 / 255.0, blue: 235 / 255.0)
+                ],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            )
+            .ignoresSafeArea()
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Greetings!")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .padding()
+                    .foregroundStyle(.purple)
+                    .background(.ultraThinMaterial)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+                    .shadow(color: .purple, radius: 10, x: 5.0, y: 5)
+                
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
