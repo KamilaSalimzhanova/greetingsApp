930f40d326ce3bad15d01678f1eed1535cb49750
kamila <ksalimzh@gmail.com>
2025-03-02 16:23:40 +0500

    remove redundancies

--- greetingsApp/ContentView.swift
+++ greetingsApp/ContentView.swift
@@ -9,33 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            LinearGradient(
-                colors: [
-                    Color(red: 230 / 255.0, green: 240 / 255.0, blue: 255 / 255.0),
-                    Color(red: 200 / 255.0, green: 230 / 255.0, blue: 255 / 255.0),
-                    Color(red: 210 / 255.0, green: 250 / 255.0, blue: 235 / 255.0)
-                ],
-                startPoint: .topLeading,
-                endPoint: .bottomTrailing
-            )
-            .ignoresSafeArea()
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Greetings!")
-                    .font(.largeTitle)
-                    .fontWeight(.semibold)
-                    .padding()
-                    .foregroundStyle(.purple)
-                    .background(.ultraThinMaterial)
-                    .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
-                    .shadow(color: .purple, radius: 10, x: 5.0, y: 5)
-                
-            }
-            .padding()
+        //ZStack {
+//            LinearGradient(
+//                colors: [
+//                    Color(red: 230 / 255.0, green: 240 / 255.0, blue: 255 / 255.0),
+//                    Color(red: 200 / 255.0, green: 230 / 255.0, blue: 255 / 255.0),
+//                    Color(red: 210 / 255.0, green: 250 / 255.0, blue: 235 / 255.0)
+//                ],
+//                startPoint: .topLeading,
+//                endPoint: .bottomTrailing
+//            )
+//            .ignoresSafeArea()
+        VStack(alignment: .leading) {
+            TextView(text: "Hello there", color: .green)
+            TextView(text: "Welcome to swift programming!", color: .orange)
+            TextView(text: "Are you ready to,", color: .yellow)
+            TextView(text: "start exploring", color: .red)
+            TextView(text: "Boom.", color: .purple)
         }
+        .padding()
+        //}
     }
 }
 
---
+++ greetingsApp/TextView.swift
@@ -0,0 +1,30 @@
+//
+//  TextView.swift
+//  greetingsApp
+//
+//  Created by kamila on 02.03.2025.
+//
+
+import SwiftUI
+
+struct TextView: View {
+    // parameters
+    let text: String
+    let color: Color
+    
+    var body: some View {
+        Text(text)
+            .fontWeight(.semibold)
+            .padding()
+            .foregroundStyle(.white)
+            .background(color.opacity(0.4))
+            .cornerRadius(/*@START_MENU_TOKEN@*/20.0/*@END_MENU_TOKEN@*/)
+            .shadow(color: color, radius: 10, x: 10, y: 10)
+    }
+}
+
+struct TextView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextView(text: "Hello there", color: .green)
+    }
+}
